e8013a94bae03b92673cfca1a72b7a8716386a45
AustinP1121 <apedigo@moreheadstate.edu>
2022-06-18 03:31:00 -0400

    problem 2 fin

--- Ch2_Prob2/Prob_2/Convert_Mile_To_Ft_Km.cs
+++ Ch2_Prob2/Prob_2/Convert_Mile_To_Ft_Km.cs
@@ -5,7 +5,7 @@
 */
 
 using System;
-using const System.Console; 
+using static System.Console; 
 
 namespace Conversion
 {
@@ -14,10 +14,13 @@ namespace Conversion
         static void Main()
         {
 
-            double miles = 4.5;
+            const double miles = 4.5;
 
-            double miles_to_ft = miles * 5280;   
-            
+            double miles_to_ft = miles * 5280;  
+            double miles_to_km = miles * 1.60934;
+
+            WriteLine("{0:F2} miles is {1:F2} kilometers and {2:F0} feet.", miles, miles_to_km, miles_to_ft);
+            Write("{0:F2} miles, {1:F2} kilometers, {2:F0} feet.", miles, miles_to_km, miles_to_ft);
             
         }
     }
