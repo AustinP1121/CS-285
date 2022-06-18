1f8b0f50f4045b3fbca1dd79015e68902da6dc3b
AustinP1121 <apedigo@moreheadstate.edu>
2022-06-18 01:43:53 -0400

    commit on 'master'

---
+++ Ch2_Prob2/Prob_2/Convert_Mile_To_Ft_Km.cs
@@ -0,0 +1,25 @@
+﻿/* 
+
+
+
+*/
+
+using System;
+using const System.Console; 
+
+namespace Conversion
+{
+    class Convert_Mile_To_Ft_Km
+    {
+        static void Main()
+        {
+
+            double miles = 4.5;
+
+            double miles_to_ft = miles * 5280;   
+            
+            
+        }
+    }
+}
+
