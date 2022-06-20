b298b22c5de15c6aaa044613aeb69b23488673db
AustinP1121 <apedigo@moreheadstate.edu>
2022-06-19 23:33:08 -0400

    fixed a few issues

--- Ch2_Prob8/Prob8/CalcGrade.cs
+++ Ch2_Prob8/Prob8/CalcGrade.cs
@@ -7,7 +7,8 @@
  June 19th, 2022
  
  Description: 
- 
+    This program calculates a student's culmative grade from a weighted average of 
+    their raw grades. After doing so, it outputs a formatted report card.
  
  */
 using System;
@@ -19,23 +20,26 @@ namespace Grades
     {
         static void Main()
         {
-
+            //constants declarations 
             const decimal homeworkWght = .10M;
             const decimal projectWght = .35M;
             const decimal quizWght = .10M;
             const decimal examWght = .30M;
             const decimal finalExamWght = .15M; 
 
+            //variable declarations
             decimal homeworkGrade = 97M;
             decimal projectGrade = 82M; 
             decimal quizGrade = 60M;
             decimal examGrade = 75M; 
             decimal finalExamGrade = 80M; 
 
+            //compute weighted grade from average
             decimal wghtedCulmGrade = ((homeworkGrade*homeworkWght) +
                 (projectGrade*projectWght) + (quizGrade*quizWght) +
                 (examGrade*examWght) + (finalExamGrade*finalExamWght))/100;  
             
+            //format and output report card
             WriteLine("\t\t***Student Report Card***\n");
             
             WriteLine("\t\t\tWeights ");
@@ -45,6 +49,7 @@ namespace Grades
                 quizWght, examWght);
             WriteLine("\t\tFinal Exam Weight: {0:P}", finalExamWght);
 
+            //grades section
             WriteLine("\n\t\t\tGrades");
             WriteLine("\t\tHomework: {0,11},\n\t\tProjects: {1,11},",
                 homeworkGrade, projectGrade);
@@ -52,10 +57,10 @@ namespace Grades
                 quizGrade, examGrade);
             WriteLine("\t\tFinal Exam: {0,9}\n", finalExamGrade);
             
+            //culmative grade
             WriteLine("\t\tCulmative Weighted Grade");
             WriteLine("\t\tGrade: {0,18:P}",wghtedCulmGrade);
-            //display the values, along with the weights
-
+            ReadKey();
         }
     }
 }
